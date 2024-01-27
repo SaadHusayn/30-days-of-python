eb0a482021d52b764e92566cd17d60840e2022f8
SaadHusayn <rsaad5095@gmail.com>
2024-01-27 12:57:51 +0500

    Added day 1

---
+++ day-1/helloworld.py
@@ -0,0 +1,28 @@
+import sys
+
+print("Task 1")
+print(f"The python version is {sys.version}\n")
+
+print("Task 2")
+print(f"3 + 4 = {3+4}")
+print(f"3 - 4 = {3-4}")
+print(f"3 * 4 = {3*4}")
+print(f"3 / 4 = {3/4}")
+print(f"3 ** 4 = {3**4}")
+print(f"3 // 4 = {3//4}\n")
+
+print("Task 3")
+print("Saad Hussai'")
+print("Shaikh")
+print("Pakistan")
+print("I am enjoying 30 days of python\n")
+
+print("Task 4")
+print(f"The type of 10 is {type(10)}")
+print(f"The type of 9.8 is {type(9.8)}")
+print(f"The type of 3.14 is {type(3.14)}")
+print(f"The type of 4-4j is {type(4-4j)}")
+print(f"The type of ['Asabeneh', 'Python', 'Finland'] is {type(['Asabeneh', 'Python', 'Finland'])}")
+print(f"The type of Saad is {type('Saad')}")
+print(f"The type of Shaikh is {type('Shaikh')}")
+print(f"The type of Pakistan is {type('Pakistan')}\n")
